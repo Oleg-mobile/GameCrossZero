8aa1873570fc0b2bbef206bfbcb3f05ff1cdcd44
OlegIvanov <juklino@yandex.ru>
2024-02-13 14:58:50 +0300

    + Создал модальное окно для игры + Реализовал механизм добавления фигуры на поле игры

--- GameApp.Mvc/wwwroot/js/views/games/index.js
+++ GameApp.Mvc/wwwroot/js/views/games/index.js
@@ -1,11 +1,17 @@
-﻿document
-    .querySelector('.cell')
-    .addEventListener('click', async () => {
+﻿
+
+document.addEventListener('DOMContentLoaded', async () => {
+
+    const gameModal = new bootstrap.Modal(document.getElementById('gameModal'));
+
+    gameModal.show();
+    document.querySelector('table').onclick = function (e) {
+        let element = e.target;
+        if (element.tagName === 'TD')
+            //element.innerText = 'крестик';
+            element.innerHTML = "<img src='/img/lobby.ico' alt='figure'/>";
+    }
+});
 
 
 
-        //const rowIndex = this.parentNode.rowIndex;
-        //const cellIndex = this.cellIndex;
-
-        //console.log(`rowIndex: ${rowIndex}, cellIndex: ${cellIndex}`);
-    });
