7fca9e990a5671d68f732834f1083b81de4019d2
OlegIvanov <juklino@yandex.ru>
2024-02-14 15:24:06 +0300

    + Добавил картинки крестика и нолика + Добавил логику вывода и смены фигур

--- GameApp.Mvc/wwwroot/js/views/games/index.js
+++ GameApp.Mvc/wwwroot/js/views/games/index.js
@@ -5,11 +5,24 @@ document.addEventListener('DOMContentLoaded', async () => {
     const gameModal = new bootstrap.Modal(document.getElementById('gameModal'));
 
     gameModal.show();
+
+    var cross = new Image();
+    cross.src = "/img/cross.ico";
+
+    var zero = new Image();
+    zero.src = "/img/zero.ico";
+
     document.querySelector('table').onclick = function (e) {
         let element = e.target;
-        if (element.tagName === 'TD')
-            //element.innerText = 'крестик';
-            element.innerHTML = "<img src='/img/lobby.ico' alt='figure'/>";
+        if (element.tagName === 'TD') {
+            if (element.style.backgroundImage.indexOf(cross.src) >= 0) {
+                element.style.backgroundImage = "url(" + zero.src + ")";
+            }
+            else {
+                //element.innerHTML = "<img src='" + cross.src + "' alt='figure'/>";
+                element.style.backgroundImage = "url(" + cross.src + ")";
+            }
+        }
     }
 });
 
