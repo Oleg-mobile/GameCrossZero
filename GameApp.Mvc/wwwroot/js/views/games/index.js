13169336bb55b0989b56ee812c38ba9f974bd75a
Oleg Ivanov <juklino@yandex.ru>
2024-04-01 00:27:52 +0300

    - Пока убрал id из модалки игры

--- GameApp.Mvc/wwwroot/js/views/games/index.js
+++ GameApp.Mvc/wwwroot/js/views/games/index.js
@@ -1,29 +1,29 @@
 ﻿
 
-const roomId = dataset.data - room - id;  // dataset is not defined
-    const gameModal = new bootstrap.Modal(document.getElementById('gameModal'));
+//const roomId = dataset.roomId;  // dataset is not defined
+const gameModal = new bootstrap.Modal(document.getElementById('gameModal'));
 
-    gameModal.show();
+gameModal.show();
 
-    let cross = new Image();
-    let zero = new Image();
+let cross = new Image();
+let zero = new Image();
 
-    cross.src = "/img/cross.ico"; 
-    zero.src = "/img/zero.ico";
+cross.src = "/img/cross.ico"; 
+zero.src = "/img/zero.ico";
 
-    document.querySelector('table').onclick = function (e) {
-        let element = e.target;
-        if (element.tagName === 'TD') {
-            if (element.style.backgroundImage.indexOf(cross.src) >= 0) {
-                element.style.backgroundImage = "url(" + zero.src + ")";
-            } else {
-                element.style.backgroundImage = "url(" + cross.src + ")";
-            }
+document.querySelector('table').onclick = function (e) {
+    let element = e.target;
+    if (element.tagName === 'TD') {
+        if (element.style.backgroundImage.indexOf(cross.src) >= 0) {
+            element.style.backgroundImage = "url(" + zero.src + ")";
+        } else {
+            element.style.backgroundImage = "url(" + cross.src + ")";
         }
     }
+}
 
-    const gameInfo = await gamesService.gameInfo(roomId);
-    document.querySelector('#whoseMove').textContent = gameInfo.WhoseMoveId;
+//const gameInfo = await gamesService.gameInfo(roomId);
+//document.querySelector('#whoseMove').textContent = gameInfo.WhoseMoveId;
 
 
 
