2a2fd46fe1aa480c01f3ce90e5768315adb4710f
Oleg Ivanov <juklino@yandex.ru>
2024-05-14 22:27:20 +0300

    + Сделал начальное заполнение поля

--- GameApp.Mvc/wwwroot/js/views/games/index.js
+++ GameApp.Mvc/wwwroot/js/views/games/index.js
@@ -5,7 +5,9 @@ const _whoseMove = document.querySelector('#whoseMove');
 const _figure = document.querySelector('#figure');
 const _roomId = _gameModalNode.dataset.roomId;
 const _gameModal = new bootstrap.Modal(_gameModalNode);
+const _table = document.querySelector('table');
 let _isMyFigureCross;
+let _myFigure;
 
 _gameModal.show();
 
@@ -15,26 +17,63 @@ let zero = new Image();
 cross.src = "/img/cross.ico"; 
 zero.src = "/img/zero.ico";
 
-document.querySelector('table').onclick = function (e) {
-    let element = e.target;
-    if (element.tagName === 'TD') {
-        if (element.style.backgroundImage.indexOf(cross.src) >= 0) {
-            element.style.backgroundImage = "url(" + zero.src + ")";
-        } else {
-            element.style.backgroundImage = "url(" + cross.src + ")";
-        }
-    }
-}
+//_table.onclick = function (e) {
+//    let element = e.target;
+//    if (element.tagName === 'TD') {
+//        if (element.style.backgroundImage.indexOf(cross.src) >= 0) {
+//            element.style.backgroundImage = "url(" + zero.src + ")";
+//        } else {
+//            element.style.backgroundImage = "url(" + cross.src + ")";
+//        }
+//    }
+//}
 
 const gameInfo = await _gamesService.getInfo(_roomId);
 
 document.querySelector('#gameId').textContent = gameInfo.gameId;
 
 _isMyFigureCross = gameInfo.isMyFigureCross;
-gameInfo.isMyStep ? _whoseMove.textContent = "Ваш" : _whoseMove.textContent = "оппонента";
-_isMyFigureCross ? _figure.textContent = "крестик" : _figure.textContent = "нолик";
 
-for (let step of gameInfo.steps) {
-    console.log(step.isCross + " - " + step.cellNumber);
+if (_isMyFigureCross === true) {
+    _figure.textContent = "крестик";
+    _myFigure = "url(" + cross.src + ")";
+} else {
+    _figure.textContent = "нолик";
+    _myFigure = "url(" + zero.src + ")";
+}
+
+//var cellsMap = _table.cells;
+var cellsMap = _table.getElementsByTagName('td');
+
+for (var i = 0; i < gameInfo.steps.length; i++) {
+    gameInfo.steps[i].isCross ?
+        cellsMap[gameInfo.steps[i].cellNumber].style.backgroundImage = "url(" + cross.src + ")" :
+        cellsMap[gameInfo.steps[i].cellNumber].style.backgroundImage = "url(" + zero.src + ")";
+}
+
+if (gameInfo.isMyStep === true) {
+    _whoseMove.textContent = "Ваш";
+
+    //for (var i = 0; i < cellsMap.length; i++) {
+    //    cellsMap[i].addEventListener('click', function () {
+    //        if (cellsMap[i].style.backgroundImage === "none") {
+    //            cellsMap[i].style.backgroundImage = _myFigure;
+    //        }
+    //    });
+    //}
+
+    _table.onclick = function (e) {
+        let element = e.target;
+        if (element.tagName === 'TD') {
+            if (element.style.backgroundImage === "") {
+                element.style.backgroundImage = _myFigure;
+            }
+        }
+    }
+
+
+} else {
+    _whoseMove.textContent = "оппонента";
+    _table.style.cursor = 'not-allowed';
 }
 
